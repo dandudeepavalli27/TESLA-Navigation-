a9f1f20023bc7e38708ef9399f3704f838c1a2cf
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:46:32 +0530

    Create Navigation.py

---
+++ Navigation.py
@@ -0,0 +1,13 @@
+# SESSION 20 – Navigation (Goal Check)
+
+# Step 1: Input values
+distance = 0.15   # distance to goal in meters
+threshold = 0.2   # goal threshold
+
+# Step 2: Check goal condition
+if distance < threshold:
+    print("Goal Reached")
+else:
+    print("Goal Not Reached")
+
+print("\nProgram Executed Successfully")
